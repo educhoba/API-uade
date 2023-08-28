375bb1c88c6da0a5909b32adb7e5040d3c5ca34b
Eduardo Guillermo Chobadindegui <edu-choba@hotmail.com>
2023-08-28 20:24:52 -0300

    test

---
+++ AdministracionAI/src/main/java/com/example/administracionai/AdministracionAiApplication.java
@@ -0,0 +1,13 @@
+package com.example.administracionai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdministracionAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdministracionAiApplication.class, args);
+    }
+
+}
---
+++ AdministracionAI/src/test/java/com/example/administracionai/AdministracionAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.administracionai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdministracionAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
