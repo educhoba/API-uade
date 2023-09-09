3fe72e7ddba8a1734a904f50cba0d0228eaeb0a4
Eduardo Guillermo Chobadindegui <edu-choba@hotmail.com>
2023-09-09 12:49:09 -0300

    Services Edificio, Imagen, Persona, Reclamo WIP

--- AdministracionAI/src/main/java/aplication/repository/IImagenRespository.java
+++ AdministracionAI/src/main/java/aplication/repository/IImagenRespository.java
@@ -1,4 +1,8 @@
 package aplication.repository;
 
-public interface IImagenRespository {
+import aplication.model.Imagen;
+import aplication.model.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IImagenRespository extends JpaRepository<Imagen,Long> {
 }
